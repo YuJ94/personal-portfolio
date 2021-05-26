d47209246b18c3ba76d0352d694cca462d5907a7
unknown <jyu8369@yahoo.com>
2021-05-26 09:21:55 -0500

    Added light / dark mode elements.

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,3 +5,23 @@ const navigation = document.querySelector(".navigation");
 window.addEventListener("scroll", function() {
     navigation.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// LIGHT DARK VARIABLES
+let body = document.body;
+let toggleBtn = document.querySelector('.toggle-btn');
+let currentTheme = localStorage.getItem('currentTheme');
+
+// LIGHT DARK FUNCTIONS
+if (currentTheme) {
+    body.classList.add('light-theme');
+}
+
+toggleBtn.addEventListener('click', function() {
+    body.classList.toggle('light-theme');
+
+    if (body.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
